49bc24bb6d0692859be8cd63a38ad48c129ad5c8
William Goode <wgoode3@gmail.com>
2019-09-10 11:00:18 -0400

    initial

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Survey.Models;
+
+namespace Survey.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost("process")]
+        public IActionResult Process(SurveyResult results)
+        {
+            ViewBag.Thing = results;
+            return View("Results");
+        }
+
+    }
+}
---
+++ Models/SurveyResult.cs
@@ -0,0 +1,12 @@
+using System;
+
+namespace Survey.Models
+{
+    public class SurveyResult 
+    {
+        public string Name {get;set;}
+        public string Location {get;set;}
+        public string Language {get;set;}
+        public string Comment {get;set;}
+    }
+}
